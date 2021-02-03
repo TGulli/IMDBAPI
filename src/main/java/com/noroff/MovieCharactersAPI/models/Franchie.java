4bf698109e2c4ed196d4c82511962bf9a08a4d2c
thomas.gulli <thomas.gulli@no.experis.com>
2021-02-03 13:19:09 +0100

    Added database

---
+++ src/main/java/com/noroff/MovieCharactersAPI/models/Franchie.java
@@ -0,0 +1,57 @@
+package com.noroff.MovieCharactersAPI.models;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Franchie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long franchie_id;
+
+    private String name;
+    private String description;
+
+    @OneToMany
+    @JoinColumn(name = "franchie_id")
+    private Set<Movie> movies; // TODO: Replace String with movie class
+
+    public Franchie() {
+
+    }
+
+    public Franchie(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.movies = movies;
+    }
+
+    public long getFranchie_id() {
+        return franchie_id;
+    }
+
+    public Set<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(Set<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
