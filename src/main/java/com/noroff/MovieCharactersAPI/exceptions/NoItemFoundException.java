24728f181d062f22e7ada58ecde24d4895688ec9
Amalie <amalie.dorthea.hunsbedt@no.experis.com>
2021-02-03 14:11:05 +0100

    Exception update

--- src/main/java/com/noroff/MovieCharactersAPI/exceptions/NoItemFoundException.java
+++ src/main/java/com/noroff/MovieCharactersAPI/exceptions/NoItemFoundException.java
@@ -1,5 +1,9 @@
 package com.noroff.MovieCharactersAPI.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NoItemFoundException extends Exception{
 
     public NoItemFoundException(String msg) {
