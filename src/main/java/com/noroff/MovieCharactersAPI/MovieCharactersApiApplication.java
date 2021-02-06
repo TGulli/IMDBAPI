d354459b0bb1d9c456ee8006fa30655c021b2468
Bjørnar Pedersen <bjornar.pedersen@no.experis.com>
2021-02-06 17:56:01 +0100

    todo list

--- src/main/java/com/noroff/MovieCharactersAPI/MovieCharactersApiApplication.java
+++ src/main/java/com/noroff/MovieCharactersAPI/MovieCharactersApiApplication.java
@@ -6,6 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieCharactersApiApplication {
 
+	/**
+	 * TODO: FIND ALTERNATIVE TO JSONIGNORE, CAUSES ISSUES WHEN UPDATING CHARACTER -> MOVIES vs MOVIES -> CHARACTERS
+	 * TODO: MAYBE LOOK AT SERVICE INSTEAD OF REPOSITORY
+	 * TODO: EXCEPTIONHANDLING FOR THE REST OF THE METHODS
+	 * TODO: SWAGGER/OPENAI
+	 * TODO: UPLOAD DB AND APP ON HEROKU
+	 */
+
+
 	public static void main(String[] args) {
 		//Hello from Git!
 		SpringApplication.run(MovieCharactersApiApplication.class, args);
