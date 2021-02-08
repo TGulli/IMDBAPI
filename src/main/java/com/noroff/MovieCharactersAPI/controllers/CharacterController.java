56a43ec81e78e8bbd3bfa69cab3dfe9f7f9c09c8
Amalie <amalie.dorthea.hunsbedt@no.experis.com>
2021-02-08 12:12:07 +0100

    update charactercontroller

--- src/main/java/com/noroff/MovieCharactersAPI/controllers/CharacterController.java
+++ src/main/java/com/noroff/MovieCharactersAPI/controllers/CharacterController.java
@@ -9,14 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping("api/v1/character")
 public class CharacterController {
+    /*
+    The CharacterController class is responsible for making endpoints,
+    handling user interactions and deciding what to do with it.
+     */
 
     @Autowired
     private CharacterRepository characterRepository;
@@ -26,22 +27,35 @@ public class CharacterController {
 
     @GetMapping
     public List<ActorCharacter> getAllCharacters(){
+        /*
+        A method to return all the characters in the database
+        as ActorCharacter objects.
+         */
         return this.characterRepository.findAll();
     }
 
     @PostMapping
     public ActorCharacter addCharacter(@RequestBody ActorCharacter character){
+        /*
+        A method to create a new ActorCharacter object and add it to the database.
+         */
         return this.characterRepository.save(character);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ActorCharacter> getById(@PathVariable(value = "id") long id) throws NoItemFoundException {
+        /*
+        A method to return a specific character by its id.
+         */
         ActorCharacter actorCharacter = characterRepository.findById(id).orElseThrow(()-> new NoItemFoundException("No character by id " + id));
         return ResponseEntity.ok().body(actorCharacter);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<ActorCharacter> deleteById(@PathVariable(value = "id") long id) throws NoItemFoundException {
+        /*
+        A method to delete a specific character and all its relationships in the database.
+         */
         ActorCharacter actorCharacter = characterRepository.findById(id).orElseThrow(()-> new NoItemFoundException("No character by id " + id));
         List<Movie> movies = actorCharacter.getMovies();
         for (Movie m: movies) {
@@ -53,6 +67,9 @@ public class CharacterController {
 
     @PutMapping("/{charid}")
     public ResponseEntity<ActorCharacter> update(@RequestBody ActorCharacter character, @PathVariable("charid") long charid) throws NoItemFoundException {
+        /*
+        A method to update a specific character and all its relationships in the database.
+         */
         ActorCharacter oldCharacter = characterRepository.findById(charid).orElseThrow(()-> new NoItemFoundException("No character by id " + character.getId()));
 
         //TODO: GRIS DETTA TE
